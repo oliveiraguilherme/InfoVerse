9eb1bd1a1e1e33e36d4573ce71236f093452eed2
Guilherme Oliveira <olivetree.silva@gmail.com>
2024-03-27 10:56:35 -0300

    Feature: first commit

---
+++ src/main/java/com/web/collect/CollectApplication.java
@@ -0,0 +1,67 @@
+package com.web.collect;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SpringBootApplication
+public class CollectApplication {
+
+	public static void main(String[] args) {
+		try{
+			Document doc = Jsoup.connect("https://www.lojamythos.com.br/hqs-livro?loja=1119494&categoria=73&categories%5B%5D=AS%2BGRANDES%2BAVENTURAS%2BDE%2BTEX").get();
+
+			Elements productNameDivs = doc.select("div.product-name");
+
+			List<String> agatexProducts = new ArrayList<>();
+
+			Pattern pattern = Pattern.compile("AGATEX.*", Pattern.CASE_INSENSITIVE);
+
+			for(Element productNameDiv : productNameDivs){
+				String productName = productNameDiv.text();
+
+				Matcher matcher = pattern.matcher(productName);
+				if(matcher.matches()){
+					agatexProducts.add(productName);
+				}
+			}
+
+			agatexProducts.sort(Comparator.naturalOrder());
+
+			System.out.println("Produtos AGATEX:");
+			for(String product : agatexProducts){
+				System.out.println(product);
+			}
+
+//			Element body = doc.body();
+//
+//			Elements divs = body.getElementsByTag("div");
+//
+//			for(Element div : divs){
+//				Elements paragraphs = div.getElementsByTag("p");
+//				if(!paragraphs.isEmpty()){
+//					System.out.println("Conteúdo dentro da div:");
+//					for(Element paragraph : paragraphs){
+//						System.out.println(paragraph.text());
+//					}
+//				}
+//			}
+		}catch (IOException ex){
+			ex.printStackTrace();
+		}
+
+
+		SpringApplication.run(CollectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/web/collect/application/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.web.collect.application.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+}
